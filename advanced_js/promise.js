4d3512055621db2c0f62105ae1d75afddcc520c8
mbbsC0DER <kprathamnegi05@gmail.com>
2025-10-23 21:16:44 +0000

    promise one

---
+++ advanced_js/promise.js
@@ -0,0 +1,5 @@
+// promise => represents the completion/failure of asyncronous operation (eg. , cryptographic opt , network response , file systems ) -->
+//Q / bluebird --> yeh libraries core js me fetch sabki functionalities sabh use kar sakte hai .
+
+// how to make promises 
+const promise_one = new Promise()
